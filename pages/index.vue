bd6224f36ec0cf3c967de296a91f5cf1670175ee
Jesse Colligan <jesse.m.colligan@gmail.com>
2020-04-26 00:37:16 -0400

    skeleton of home page, svgs not rendering yet not sure why

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <v-col cols="12">
 
 
-        <v-row class="first-row">
+        <v-row id="header" class="first-row">
           
           <v-col cols="8">
             <div class="first-row-copy">
@@ -29,22 +29,76 @@
 
 
         <!-- How It Works section -->
-        <v-row class="d-block">
+        <v-row class="d-block" id="how-it-works">
           <h2>How it Works</h2>
 
-          <v-row>
-            <ul v-for="(card, i) in howItWorksCards" :key="i">
-              <v-col cols="3">
-                <li>
-                  <div class="card-number">{{i}}</div>
-                  <img :src="card.img" alt="">
+          <v-row >
+            <!-- need to refactor colunns -->
+              <v-col cols="3" v-for="(card, i) in howItWorksCards"  :key="i">
 
-                </li>
+                  <div class="card-number">{{i + 1}}</div>
+
+                  <!-- why is this card.img not working?  worked w random internet image url but not relative svg file path  -->
+                  <img :src="card.img" :alt="card.title">
+                  <h3>{{card.title}}</h3>
+                  <div>{{card.body}}</div>
+
               </v-col>
-            </ul>
+
           </v-row>
+
+          <v-btn class="primary-1--text">
+            <nuxt-link to="/how-it-works">
+              Learn More
+            </nuxt-link>
+          </v-btn>
         </v-row>
 
+
+        <!-- Why Covid Watch -->
+        <v-row id="why-covid-watch">
+            <v-col cols="4" v-for="(card, i) in whyCards" :key="i">
+              <div>
+                 <!-- why is this card.img not working?  worked w random internet image url but not relative svg file path  -->
+                  <img :src="card.img" :alt="card.title">
+                  <h3>{{card.title}}</h3>
+                  <div>{{card.body}}</div>
+              </div>
+            </v-col>
+        </v-row>
+
+
+        <!-- Latest News -->
+        <v-row id="latest-news"> 
+
+          <!-- cols slightly uneven -->
+          <v-col cols="12">
+            <v-row class="d-flex align-center justify-space-between">
+              <h2>Latest News</h2>
+
+              <nuxt-link to="/news">More News</nuxt-link>
+
+            </v-row>
+          </v-col>
+
+        </v-row>
+
+
+        <!-- Download the App -->
+        <v-row id="download-app"> 
+
+          <v-col cols="4">
+          <h1 class="secondary_2--text ">Download the App</h1>
+           <div>
+                <v-btn>Download Android Beta</v-btn>
+                <v-btn>Download iOS Beta</v-btn>
+            </div>
+          </v-col>
+
+          <v-col cols="7" v-for="(img, i) in mobileImages" :key="i">
+            <img :src="img" alt="mobile_preview">
+          </v-col>
+        </v-row>
 
       </v-col>
     </v-row>
@@ -55,8 +109,10 @@
 
 <style lang="scss">
   #home {
-    ul {
-      list-style: none;
+    img {
+      // placeholder to see the img elements
+      width: 200px;
+      height: 200px;
     }
   }
 </style>
@@ -68,37 +124,45 @@ export default {
       {
         title: "Privacy First",
         body: "Sam and Jane’s interaction is logged on their phones anonymously.",
-        img: "https://i.ytimg.com/vi/PRwvS-SNHuQ/maxresdefault.jpg"
+        img: "../assets/home_page/how_it_works_1.svg"
       },
       {
         title: "Diagnosis Reports",
         body: "Sam later tests postive for COVID-19 and he enters his results into the secure Covid Watch system.",
-        img: "https://i.ytimg.com/vi/PRwvS-SNHuQ/maxresdefault.jpg"
+        img: "../assets/home_page/how_it_works_2.svg"
       },
       {
         title: "Contact Alerts",
         body: "Jane’s phone gets a notification that a recent contact has tested positive for COVID-19.",
-        img: "https://i.ytimg.com/vi/PRwvS-SNHuQ/maxresdefault.jpg"
+        img: "../assets/home_page/how_it_works_3.svg"
       },
       {
         title: "Community Safety",
         body: "Jane and Sam helped keep their communities safe. They share the app so that others can, too.",
-        img: "https://i.ytimg.com/vi/PRwvS-SNHuQ/maxresdefault.jpg"
+        img: "../assets/home_page/how_it_works_4.svg"
       },
     ],
     whyCards: [
       {
         title: "No tracking, ever",
-        body: "No personal information is shared. No GPS locations are stored. Bluetooth signals anonymously log when you've been near another phone with Covid Watch installed."
+        body: "No personal information is shared. No GPS locations are stored. Bluetooth signals anonymously log when you've been near another phone with Covid Watch installed.",
+        img: "../assets/home_page/why_covid_watch_1.svg"
       },
       {
         title: "Easier than staying home",
-        body: "You can leave your home and help keep your community healthy just by installing the app. You don't even need to keep the app open for it to protect you."
+        body: "You can leave your home and help keep your community healthy just by installing the app. You don't even need to keep the app open for it to protect you.",
+        img: "../assets/home_page/why_covid_watch_2.svg"
       },
       {
         title: "Share with each other",
-        body: "Trust comes when we work together. Covid Watch is a non-profit on a mission to protect your health and privacy, not big tech or government."
+        body: "Trust comes when we work together. Covid Watch is a non-profit on a mission to protect your health and privacy, not big tech or government.",
+        img: "../assets/home_page/why_covid_watch_3.svg"
       }
+    ],
+    mobileImages: [
+      "../assets/home_page/mobile_1.svg",
+      "../assets/home_page/mobile_2.svg",
+      "../assets/home_page/mobile_3.svg"
     ]
   })
 }
