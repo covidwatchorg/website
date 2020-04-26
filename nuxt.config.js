a7bcbef955a1bc62e434e656472490404c5167d4
Tessa Alexanian <hello@tessa.fyi>
2020-04-25 22:09:13 -0700

    Add CSS colors to vuetify theme

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,16 +56,21 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
       themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
+        light: {
+          primary: "#2C58B1",
+          bluejay: "#2C58B1",
+          accent: "#F05452",
+          tangerine: "#F05452",
+          secondary: "#43C4D9",
+          aqua: "#43C4D9",
+          plum: "#4B0A70",
+          lavender: "#A680B7",
+          elderberry: "#2C007B",
+          info: "#2C58B1",    // bluejay
+          warning: "#2C58B1", // tangerine
+          error: "#F05452",   // tangerine
+          success: "#A680B7"  // lavender     
         }
       }
     }
