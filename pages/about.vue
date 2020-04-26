8c6d298c52e5932a165dd0e3a415f156568aa83a
Colby Morrison <colbyamorrison@gmail.com>
2020-04-26 16:58:30 -0700

    updates still in progress

--- pages/about.vue
+++ pages/about.vue
@@ -1,76 +1,97 @@
 <template>
     <v-container grid-list-xs>
-<v-row id="about">
-        <v-col cols="12">
+        <v-row id="about">
+            <v-col cols="12">
 
-            <v-row id="header" class="first-row">
+                <v-row id="header" class="first-row">
 
-                <v-col cols="7">
-                    <div class = "first-row-copy">
-                        <h1>About Us</h1>
-                        <p>Covid Watch is a <b>nonprofit</b>, global effort to give you the power to reduce the spread of COVID-19 in the palm of your hand.</p>
-                        <p>We build and foster the adoption of decentralized technology that preserves your right to <b>privacy</b> while <b>protecting</b> your community.</p>
-                        <p>We want to speed our return to a world where we can be <b>together</b> again, safely and securely.</p>
-                    </div>
-                </v-col>
+                    <v-col cols="7">
+                        <div class = "first-row-copy">
+                            <h1>About Us</h1>
+                            <p>Covid Watch is a <b>nonprofit</b>, global effort to give you the power to reduce the spread of COVID-19 in the palm of your hand.</p>
+                            <p>We build and foster the adoption of decentralized technology that preserves your right to <b>privacy</b> while <b>protecting</b> your community.</p>
+                            <p>We want to speed our return to a world where we can be <b>together</b> again, safely and securely.</p>
+                        </div>
+                    </v-col>
 
-                <v-col cols="4">
-                    <!-- Globe and people image --> 
-                    <!-- img src="../assets/blah"-->
-                </v-col>
-
-
-                <v-col cols="3">
-                    <v-btn class="primary-1--text">
-                        <!-- Where should this link to-->
-                        <nuxt-link to="/collaborate">
-                            Partner With Us
-                        </nuxt-link>
-                    </v-btn>
-                </v-col>   
+                    <v-col cols="4">
+                        <!-- Globe and people image --> 
+                        <!-- img src="../assets/blah"-->
+                    </v-col>
 
 
-                <v-col cols="3">
-                    <v-btn class="primary-1--text">
-                        <nuxt-link to="/collaborate">
+                    <v-col cols="3">
+                        <v-btn class="primary-1--text">
                             <!-- Where should this link to-->
-                            Use Our Solutions
-                        </nuxt-link>
-                    </v-btn>
+                            <nuxt-link to="/collaborate">
+                                Partner With Us
+                            </nuxt-link>
+                        </v-btn>
+                    </v-col>   
 
-                </v-col>
 
-            </v-row>
-        </v-col>
-    </v-row>
+                    <v-col cols="3">
+                        <v-btn class="primary-1--text">
+                            <nuxt-link to="/collaborate">
+                                <!-- Where should this link to-->
+                                Use Our Solutions
+                            </nuxt-link>
+                        </v-btn>
 
-    <v-row id="we-value">
-        <!-- TODO -->
-    </v-row>
+                    </v-col>
 
-    <v-row id="join-us">
+                </v-row>
+            </v-col>
+        </v-row>
+
+        <v-row id="we-value">
+            <!-- TODO -->
+        </v-row>
+
+        <v-row id="join-us">
             <v-col cols="2">
                 <h2>Join Us</h2>
             </v-col>
 
 
             <v-col cols="8">
-            <!--- People biking -->
+                <!--- People biking -->
             </v-col> 
 
-            <v-col cols="3" v-for="(cta, i) in callsToAction">
+            <v-col cols="3" v-for="(cta, i) in callsToAction" :key="i">
                 <!-- should the buttons be components like this? >
-                <!-- primary-button text=cta.button_text link="/donate"></primary-button -->
+                    <!-- primary-button text=cta.button_text link="/donate"><!-- /primary-button -->
 
-                <div>{{cta.text}} </div>
+                    <div>{{cta.cta_text}}</div>
 
 
             </v-col>
-    </v-row>
+        </v-row>
+
+        <v-row id="our-team">
+            <v-col cols="12">
+                <div class="our-team-copy">
+                    <h2>Our Team</h2>
+                    <p>Covid Watch is a group of more than 400 volunteers from around the world. We are scientific researchers, privacy and public health experts, journalists, developers, and designers.</p>
+                </div>
+            </v-col>
+
+            <v-col cols="6" v-for="(founder,i) in founders" :key="i">
+                <!--TODO -->
+            </v-col>
+        </v-row>
+
+            <v-row id="academic-partners">
+
+            </v-row>
+
+            <v-row id="collaborator-projects">
+            </v-row>
 
 
 
-</v-container>
+
+    </v-container>
 </template>
 
 <script>
