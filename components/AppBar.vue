596873c3296730a1eb1d5fb2b7395cde599aaf42
Tessa Alexanian <hello@tessa.fyi>
2020-04-26 12:52:39 -0700

    Create AppBar component

---
+++ components/AppBar.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app-bar app flat clipped-left>
+    <v-container>
+      <v-row>
+        <v-toolbar-title v-text="title" />
+        <v-spacer />
+        <!--Desktop menu-->
+        <v-toolbar-items class="d-none d-md-flex">
+          <v-btn
+            v-for="link in navLinks"
+            :key="link.title"
+            :title="link.title"
+            :to="link.href"
+            nuxt
+            text
+            >{{ link.title }}</v-btn
+          >
+        </v-toolbar-items>
+        <!--Mobile menu-->
+        <v-toolbar-items class="d-flex d-md-none">
+          <v-menu offset-y>
+            <template v-slot:activator="{ on }">
+              <v-btn text v-on="on">
+                <v-icon>mdi-menu</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item v-for="link in navLinks" :key="link.title">
+                <nuxt-link class="link primary--text" :to="link.href">{{
+                  link.title
+                }}</nuxt-link>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </v-toolbar-items>
+      </v-row>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "AppBar",
+  props: {
+    navLinks: Array,
+    title: String
+  }
+};
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,40 +1,7 @@
 <template>
   <v-app>
     <!--Fixed top navbar-->
-    <v-app-bar app flat clipped-left>
-      <v-container>
-        <v-row>
-          <v-toolbar-title v-text="title" />
-          <v-spacer />
-          <!--Desktop menu-->
-          <v-toolbar-items class="d-none d-md-flex">
-            <v-btn
-              v-for="link in navLinks"
-              :key="link.title"
-              :title="link.title"
-              :to="link.href"
-              nuxt
-              text
-            >{{ link.title }}</v-btn>
-          </v-toolbar-items>
-          <!--Mobile menu-->
-          <v-toolbar-items class="d-flex d-md-none">
-            <v-menu offset-y>
-              <template v-slot:activator="{ on }">
-                <v-btn text v-on="on">
-                  <v-icon>mdi-menu</v-icon>
-                </v-btn>
-              </template>
-              <v-list>
-                <v-list-item v-for="link in navLinks" :key="link.title">
-                  <nuxt-link class="link primary--text" :to="link.href">{{ link.title }}</nuxt-link>
-                </v-list-item>
-              </v-list>
-            </v-menu>
-          </v-toolbar-items>
-        </v-row>
-      </v-container>
-    </v-app-bar>
+    <AppBar :navLinks="navLinks" :title="title"></AppBar>
 
     <!--Main content, pulled in by nuxt. Thanks, nuxt!-->
     <v-content>
@@ -51,7 +18,9 @@
             <b>{{ section.title }}</b>
             <v-list>
               <v-list-item v-for="link in section.links" :key="link.title">
-                <nuxt-link class="link primary--text" :to="link.href">{{ link.title }}</nuxt-link>
+                <nuxt-link class="link primary--text" :to="link.href">{{
+                  link.title
+                }}</nuxt-link>
               </v-list-item>
             </v-list>
           </v-col>
@@ -66,7 +35,11 @@
 </template>
 
 <script>
+import AppBar from "../components/AppBar.vue";
 export default {
+  components: {
+    AppBar
+  },
   data() {
     return {
       navLinks: [
