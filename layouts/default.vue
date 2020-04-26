cc832a880088d68c28f7f3fd16aca16a374e7938
Tessa Alexanian <hello@tessa.fyi>
2020-04-25 22:55:58 -0700

    default navbar with all the right things, no style

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,90 +1,82 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+  <v-app>
+    <!--Fixed navbar-->
     <v-app-bar
-      :clipped-left="clipped"
-      fixed
       app
+      flat
+      clipped-left
     >
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn
-        icon
-        @click.stop="miniVariant = !miniVariant"
-      >
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="clipped = !clipped"
-      >
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn
-        icon
-        @click.stop="fixed = !fixed"
-      >
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn
-        icon
-        @click.stop="rightDrawer = !rightDrawer"
-      >
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
-    </v-app-bar>
-    <v-content>
       <v-container>
-        <nuxt />
+        <v-layout row>
+          <v-toolbar-title v-text="title" />
+          <v-spacer />
+          <!-- Desktop menu -->
+          <v-toolbar-items class="d-none d-md-flex">
+            <v-btn
+              v-for="link in navLinks"
+              :key="link.title"
+              :title="link.title"
+              :to="link.href"
+              nuxt
+              text
+            >{{ link.title }}</v-btn>
+          </v-toolbar-items>
+          <!-- Mobile menu -->
+          <v-toolbar-items class="d-flex d-md-none">
+            <v-menu offset-y >
+              <template v-slot:activator="{ on }">
+                <v-btn text v-on="on">
+                  <v-icon>mdi-menu</v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item
+                  v-for="link in navLinks"
+                  :key="link.title"
+                >
+                  <nuxt-link
+                    class="link primary--text"
+                    :to="link.href"
+                  >
+                      {{ link.title }}
+                  </nuxt-link>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-toolbar-items>
+        </v-layout>
       </v-container>
-    </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
+    </v-app-bar>
+
+      <v-content>
+        <v-container>
+          <nuxt />
+        </v-container>
+      </v-content>
+
+      <v-navigation-drawer
+        v-model="rightDrawer"
+        :right="right"
+        temporary
+        fixed
+      >
+        <v-list>
+          <v-list-item @click.native="right = !right">
+            <v-list-item-action>
+              <v-icon light>
+                mdi-repeat
+              </v-icon>
+            </v-list-item-action>
+            <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-footer
+        :fixed="fixed"
+        app
+      >
+        <span>&copy; {{ new Date().getFullYear() }}</span>
+      </v-footer>
   </v-app>
 </template>
 
@@ -92,25 +84,25 @@
 export default {
   data () {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
+      navLinks: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
+          title: "How It Works",
+          href: "/how-it-works",
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
+          title: "FAQ",
+          href: "/faq",
+        },
+        {
+          title: "About",
+          href: "/about",
+        },
+        {
+          title: "News",
+          href: "/news",
+        },
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Covid Watch'
     }
   }
 }
