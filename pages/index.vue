31fbb2ee8416b48b64e4360ed01f81dfede491db
Tessa Alexanian <alexanian@users.noreply.github.com>
2020-04-26 23:35:23 -0700

    Initial typography styles, plus a Button component (#2)
    ## Summary
    
    Moving towards having typography / button components that look like what we want, including:
    * Headers are the right size and colour, though I feel like I should try pulling typography into its own scss file?
    * Button component- see `index.vue` for usage examples
    * Am now auto-formatting on save with Prettier
    
    ## Screenshot
    
    ![screenshot of usage examples](https://user-images.githubusercontent.com/7595169/80340088-dc010800-8814-11ea-8928-53899857730f.png)

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,26 @@
 <template>
-  <h1>home rendered</h1>
+  <v-container>
+    <h1>h1: home rendered</h1>
+    <h2>h2: let's test</h2>
+    <h3>h3: header typography</h3>
+    <p class="title">title: and typography</p>
+    <p class="title-caps">title caps</p>
+    <p class="subtitle">subtitle: in general</p>
+    <p>default</p>
+    <p class="caption">caption</p>
+    <Button>A default button</Button>
+    <Button secondary>A secondary button</Button>
+    <Button disabled large>A disabled button</Button>
+    <Button primary hero nuxt to="/support">A hero button w/ link</Button>
+  </v-container>
 </template>
 
+<script>
+import Button from "../components/Button.vue";
+
+export default {
+  components: {
+    Button
+  }
+};
+</script>
