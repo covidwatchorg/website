b68baf411dc0eca1957dd778d0ed9e9f9300343a
Tessa Alexanian <hello@tessa.fyi>
2020-04-26 23:13:51 -0700

    functional, though incompletely styled, button

---
+++ components/Button.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-btn
+    depressed
+    color="primary"
+    :class="{ hero: hero }"
+    :disabled="disabled"
+    :nuxt="nuxt"
+    :outlined="secondary"
+    :to="to"
+  >
+    <slot></slot>
+  </v-btn>
+</template>
+
+<style lang="scss" scoped>
+.hero {
+  height: 58px;
+}
+</style>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    hero: Boolean,
+    disabled: Boolean,
+    nuxt: Boolean,
+    secondary: Boolean,
+    to: String
+  }
+};
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -7,5 +7,19 @@
     <p class="subtitle">subtitle: in general</p>
     <p>default</p>
     <p class="caption">caption</p>
+    <Button>A default button</Button>
+    <Button secondary>A secondary button</Button>
+    <Button disabled large>A disabled button</Button>
+    <Button primary hero nuxt to="/support">A hero button w/ link</Button>
   </v-container>
 </template>
+
+<script>
+import Button from "../components/Button.vue";
+
+export default {
+  components: {
+    Button
+  }
+};
+</script>
