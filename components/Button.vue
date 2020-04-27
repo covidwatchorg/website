860528103b989cd2ecbff7fb7d46d4a47f0f1644
Colby Morrison <colbyamorrison@gmail.com>
2020-04-26 22:12:30 -0700

    Button component

---
+++ components/Button.vue
@@ -0,0 +1,38 @@
+<template>
+    <v-btn class="cta-button" :class="type" :nuxt="true" :to="link">
+        {{ text }}
+    </v-btn>
+</template>
+
+
+<script>
+export default {
+    name: "Button",
+    props: {
+        type: {
+            type: String,
+            required: true
+        },
+        link:{
+            type: String,
+            required: true
+        },
+        text:{
+            type: String,
+            required: true
+        }
+    }
+};
+</script>
+
+
+<style lang="scss">
+    .cta-button {
+        width: 300px;
+        font-family: Montserrat;
+        font-style: normal;
+        font-weight: bold;
+        // this font size from figma makes text look HUGE relative to button and other elements
+        // font-size: 18px;
+    }
+</style>
