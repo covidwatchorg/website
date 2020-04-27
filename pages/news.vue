07bfd205d0ad0ff61dda300c1981592e444861a5
Jesse Colligan <jesse.m.colligan@gmail.com>
2020-04-27 17:20:39 -0400

    basic filtering with typerror still

--- pages/news.vue
+++ pages/news.vue
@@ -4,50 +4,30 @@
       <v-col>
         <v-row id="hero">
           
-          <v-col cols="7">
+          <v-col cols=7>
             <h1>News and Media</h1>
             <div class="mt-10 d-flex justify-space-between align-center" style="max-width:500px;">
               <p class="title">Filter by:</p> 
-              <p @click="showAll" class="filter">All News</p>
-              <p @click="showPressReleases" class="filter">Press Releases</p>
-              <p @click="showPressMentions" class="filter">Press Mentions</p>
+              <p @click="showAll" class="filter px-2">All News</p>
+              <p @click="showPressReleases" class="filter px-2">Press Releases</p>
+              <p @click="showPressMentions" class="filter px-2">Press Mentions</p>
             </div>
           </v-col>
 
-          <v-col cols="4">
+          <v-spacer></v-spacer>
+
+          <v-col cols=4>
             <img src="../assets/news_page/bench_dude.svg" alt="news">
           </v-col>
         </v-row>
 
-
+        <!-- all news cards section -->
         <v-row id="news-cards">
 
-          <!-- modularized version -->
+          <!-- modularized NewsCard version -->
           <v-col cols="4" v-for="(card,i) in cardsToRender" :key="i"> 
               <NewsCard :card="card"></NewsCard>
-          </v-col>
-
-
-          <!-- non-modularized version (to be removed, just for reference currently just in case) -->
-            <!-- <v-col cols="4" v-for="(card,i) in newsCards" :key="i">  -->
-              <!-- <NewsCard type="card.type" date="card.date" url="card.url" title="card.title" author="card.author" outlet="card.outlet"></NewsCard> -->
-                    <!-- <v-card class="news-card">
-                          <img style="position:absolute;" src="../assets/home_page/news_cards_top_accent.svg" alt="stripe">
-                          <div class="d-flex py-6 px-6 justify-space-between">
-                            <img src="../assets/home_page/news_icon.svg" alt=news>
-                            <div class="news-date">{{card.date}}</div>
-                          </div>
-                          <div class="pa-6">
-                            <a :href="card.url" class="primary--text" style="text-decoration:none;font-weight:bold;">{{card.title}} <img style="height:10px;" src="../assets/home_page/arrow_icon.svg" alt="arrow"></a>
-                            <div class="primary--text float-right mt-10">
-                              - {{card.author}}, <span style="font-style:italic;"> {{card.outlet}} </span>
-                            </div>
-                        </div>
-                    </v-card> -->
-                    <!-- replace w NewsCard component above -->
-            <!-- </v-col> -->
-
-            
+          </v-col>            
 
         </v-row>
       </v-col>
@@ -63,11 +43,11 @@
 
     .filter {
       font-weight: 500;
-      color: "primary";
+      color: var(--v-primary-base);
     }
 
     .filter:hover {
-      color: #F05452;
+      color: var(--v-aqua-base);
       cursor: pointer;
     }
   }
@@ -82,19 +62,20 @@ export default {
   components: {
     NewsCard
   },
-    mounted() {
-      this.cardsToRender = this.newsCards.slice();
-    },
+  mounted() {
+    this.cardsToRender = this.newsCards.slice();
+  },
   methods: {
+    // all these methods currently generate errors "TypeError: Cannot read property 'filter' of undefined".
+    // I suspect it is related to some sort of inner working of Vue related to passing data around that I'm not aware of
     showAll: () => {
-      debugger
       this.cardsToRender = this.newsCards.slice();
     },
     showPressReleases: () => {
-      this.cardsToRender = this.newsCards.filter(card => card.type ==="press_release");
+      this.cardsToRender = this.filter(card => card.type === "press_release");
     },
     showPressMentions: () => {
-      this.cardsToRender = this.newsCards.filter(card => card.type ==="press_mention");
+      this.cardsToRender = this.newsCards.filter(card => card.type === "press_mention");
     }
   },
   head() {
@@ -102,6 +83,12 @@ export default {
       title: "News | Covid Watch"
     };
   },
+  // this was a hacky attempt to resolve the errors w the methods above
+  // computed: {
+  //   newsCardsCopy() {
+  //     return { data:newsCards }
+  //   }
+  // },
   data: () => ({
     cardsToRender: undefined,
     newsCards: [
