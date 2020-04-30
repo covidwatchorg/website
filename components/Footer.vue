d59ecbc1b3eb5d07378f757b5e8369f5ca3f12fb
Tessa Alexanian <hello@tessa.fyi>
2020-04-29 23:43:08 -0700

    add social links to footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,31 +2,34 @@
   <v-footer app absolute color="primary" style="white--text" dark>
     <v-container>
       <v-row>
-        <v-col
-          cols="12"
-          sm="6"
-          md="2"
-          v-for="section in sections"
-          :key="section.title"
-        >
+        <v-col cols="12" sm="6" md="2" v-for="section in sections" :key="section.title">
           <b>{{ section.title }}</b>
           <v-list class="footer-link-list" dense>
-            <v-list-item
-              v-for="link in section.links"
-              :key="link.title"
-              class="pl-0"
-            >
-              <nuxt-link class="link" :to="link.href">{{
+            <v-list-item v-for="link in section.links" :key="link.title" class="pl-0">
+              <nuxt-link class="link" :to="link.href">
+                {{
                 link.title
-              }}</nuxt-link>
+                }}
+              </nuxt-link>
             </v-list-item>
           </v-list>
         </v-col>
         <v-spacer />
         <v-col :md="2">
-          <Button class="primary--text" nuxt to="/support" color="white"
-            >Donate</Button
-          >
+          <Button class="primary--text" nuxt to="/support" color="white">Donate</Button>
+          <v-row justify="space-around" class="mb-2">
+            <span class="group mt-8 pa-2">
+              <a href="https://github.com/covid19risk" class="ma-3">
+                <v-icon large dark>mdi-github</v-icon>
+              </a>
+              <a href="https://www.facebook.com/CovidWatch2020" class="ma-3">
+                <v-icon large dark>mdi-facebook</v-icon>
+              </a>
+              <a href="https://twitter.com/COVIDWatchApp" class="ma-3">
+                <v-icon large dark>mdi-twitter</v-icon>
+              </a>
+            </span>
+          </v-row>
         </v-col>
         <v-col cols="12" class="text-center">
           <span class="caption">
@@ -39,7 +42,7 @@
   </v-footer>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .footer-link-list {
   &.v-list {
     background-color: transparent;
@@ -49,6 +52,10 @@
     font-size: 16px;
     color: white;
     text-decoration: none;
+
+    &:hover {
+      color: var(--v-aqua-base);
+    }
   }
 }
 </style>
