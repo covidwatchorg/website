d788a099842444c97663cf87cbda9e88b71cb493
Colby Morrison <colbyamorrison@gmail.com>
2020-04-30 11:19:02 -0700

    how it works wholesale refactor for reactivity

--- components/Step.vue
+++ components/Step.vue
@@ -1,21 +1,46 @@
 <template>
-    <v-col cols="3">
-        <img :src="img">
-        <h3 class="mb-4" {{ title }} </h3>
-        <p class="mb-5">
-        {{ text }}
-        </p>
-    </v-col>
+    <div>
+        <v-row 
+            v-if="$vuetify.breakpoint.smAndDown"
+            class="py-5 px-12 d-flex align-end"
+            > 
+            <v-col cols="10">
+                <img :src="img_step">
+            </v-col>
+            <v-col cols="8">
+                <img :src="img_num">
+                <h3 class="mb-4" {{ title }} </h3>
+                <p class="mb-5">
+                {{ text }}
+                </p>
+            </v-col>
+        </v-row>
+
+        <v-row v-else class="py-5 px-12 d-flex align-end" :class="{ 'justify-end' : right }">
+            <v-col cols="3">
+                <img :src="img_num">
+                <h3 class="mb-4" {{ title }} </h3>
+                <p class="mb-5">
+                {{ text }}
+                </p>
+            </v-col>
+
+            <v-col cols="3">
+                <img :src="img_step">
+            </v-col>
+        </v-row>
+    </div>
 </template>
 
 <script>
-
 export default {
     name: "Step",
     props: {
-        img: String, 
+        img_num: String, 
+        img_step: String,
         title: String,
         text: String,
+        right: Boolean,
     },
 };
 </script>
--- pages/how-it-works.vue
+++ pages/how-it-works.vue
@@ -24,119 +24,32 @@
 
             <!-- Step 1 -->
             <div class="triangle_right_1">
-            <v-row class="py-5 px-12 d-flex align-end" id="step-1">
-                <Step :img="require('../assets/how_it_works/num-1.svg')" title="Bluetooth signals" text="After Jane installs Covid Watch, her phone begins to chirp random numbers using Bluetooth. Her phone also listens for chirps sent by other phones nearby."></Step>
-                <!-- <v-col cols="3">
-                    <img class="mb-4" src="../assets/how_it_works/num-1.svg">
-                    <h3 class="mb-5"> Bluetooth signals </h3>
-                    <p>
-                    After Jane installs Covid Watch, her phone begins to chirp random numbers using Bluetooth. Her phone also listens for chirps sent by other phones nearby.
-                    </p>
-                </v-col> -->
-                <v-col cols="3">
-                    <img src="../assets/how_it_works/step-1.svg">
-                </v-col>
-            </v-row>
+                    <Step :class="justify-end" :img_num="require('../assets/how_it_works/num-1.svg')" :img_step="require('../assets/how_it_works/step-1.svg')" title="Bluetooth signals" text="After Jane installs Covid Watch, her phone begins to chirp random numbers using Bluetooth. Her phone also listens for chirps sent by other phones nearby."></Step>
 
             <!-- Step 2 -->
             <div class="triangle_left_1">
-            <v-row class="py-5 px-12 d-flex align-end" justify="end" id="step-2">
-                <Step :img="require('../assets/how_it_works/num-2.svg')" title="No Tracing or Tracking"  text="The random numbers change at short intervals, so Jane’s identity and location can’t be determined by nearby phones that can hear the chirps."></Step>
-                <!-- <v-col cols="3">
-                    <img class="mb-4" src="../assets/how_it_works/num-2.svg">
-                    <h3 class="mb-5"> No Tracing or Tracking </h3>
-                    <p>
-                    The random numbers change at short intervals, so Jane’s identity and location can’t be determined by nearby phones that can hear the chirps.
-                    </p>
-                </v-col> -->
-                <v-col cols="3">
-                    <img src="../assets/how_it_works/num-2-img.svg">
-                </v-col>
-            </v-row>
+                <Step right :img_num="require('../assets/how_it_works/num-2.svg')" :img_step= "require('../assets/how_it_works/num-2-img.svg')" title="No Tracing or Tracking"  text="The random numbers change at short intervals, so Jane’s identity and location can’t be determined by nearby phones that can hear the chirps."></Step>
+                
             
-            <!-- <img class="traingle_image_right pr-10" src="../assets/how_it_works/triangle-2.svg"> -->
-
             <!-- Step 3 -->
-            <v-row class="py-5 px-12 d-flex align-end" id="step-3">
-                <Step :img="require('../assets/how_it_works/num-3.svg')" title=" A Chance Encounter"  text="Jane has a chat with Sam, who also has the Covid Watch app installed. Their phones log these random numbers and the time."></Step>
-                <!-- <v-col cols="3">
-                    <img class="mb-4" src="../assets/how_it_works/num-3.svg">
-                    <h3 class="mb-5">  A Chance Encounter </h3>
-                    <p>
-                    Jane has a chat with Sam, who also has the Covid Watch app installed. Their phones log these random numbers and the time.
-                    </p>
-                </v-col> -->
-                <v-col cols="3">
-                    <img src="../assets/how_it_works/num-3-img.svg">
-                </v-col>
-            </v-row>
+              <Step :img_num="require('../assets/how_it_works/num-3.svg')" :img_step="require('../assets/how_it_works/num-3-img.svg')" title=" A Chance Encounter"  text="Jane has a chat with Sam, who also has the Covid Watch app installed. Their phones log these random numbers and the time."></Step>
 
             <!-- Step 4 -->
-            <v-row class="py-5 d-flex align-end" justify="end" id="step-2">
-                <Step :img="require('../assets/how_it_works/num-4.svg')" title="An Anonymous Log"  text="Jane’s phone keeps a log of these numbers for two to four weeks. No personal information is needed for the system to work! Just a log of random numbers along with time. "></Step>
-                <!-- <v-col cols="3">
-                    <img class="mb-4" src="../assets/how_it_works/num-4.svg">
-                    <h3 class="mb-5"> An Anonymous Log </h3>
-                    <p>
-                    Jane’s phone keeps a log of these numbers for two to four weeks. No personal information is needed for the system to work! Just a log of random numbers along with time. 
-                    </p>
-                </v-col> -->
-                <v-col cols="3">
-                    <img src="../assets/how_it_works/num-4-img.svg">
-                </v-col>
-            </v-row>
+                <Step right :img_num="require('../assets/how_it_works/num-4.svg')" :img_step="require('../assets/how_it_works/num-4-img.svg')" title="An Anonymous Log"  text="Jane’s phone keeps a log of these numbers for two to four weeks. No personal information is needed for the system to work! Just a log of random numbers along with time. "></Step>
             </div>
 
 
             <!-- Step 5 -->
             <div class="triangle_right_2">
-            <v-row class="py-5 px-12 d-flex align-end" id="step-2">
-                <Step :img="require('../assets/how_it_works/num-5.svg')" title="Voluntary Reports"  text="A few days later, Sam tests positive for COVID-19. Sam chooses to report using a verification code in the app. When he does, his log of random numbers is shared with the Covid Watch server."></Step>
-                <!-- <v-col cols="3">
-                    <img class="mb-4"  src="../assets/how_it_works/num-5.svg">
-                    <h3 class="mb-5"> Voluntary Reports </h3>
-                    <p>
-                    A few days later, Sam tests positive for COVID-19. Sam chooses to report using a verification code in the app. When he does, his log of random numbers is shared with the Covid Watch server.
-                    </p>
-                </v-col> -->
-                <v-col cols="3">
-                    <img src="../assets/how_it_works/num-5-img.svg">
-                </v-col>
-            </v-row>
+                <Step :img_num="require('../assets/how_it_works/num-5.svg')" :img_step="require('../assets/how_it_works/num-5-img.svg')" title="Voluntary Reports"  text="A few days later, Sam tests positive for COVID-19. Sam chooses to report using a verification code in the app. When he does, his log of random numbers is shared with the Covid Watch server."></Step>
 
             <!-- Step 6 -->
             <div class="triangle_left_2">
-            <v-row class="py-5 px-12 d-flex align-end" justify="end" id="step-2">
-                <Step :img="require('../assets/how_it_works/num-6.svg')" title="Exposure Notifications"  text="Jane’s phone checks the Covid Watch server for numbers uploaded by COVID-19 positive patients. When Sam shares his result, Jane receives an alert that she may have been exposed to COVID-19."></Step>
-                <!-- <v-col cols="3">
-                    <img class="mb-4" src="../assets/how_it_works/num-6.svg">
-                    <h3 class="mb-5" > Exposure Notifications </h3>
-                    <p>
-                    Jane’s phone checks the Covid Watch server for numbers uploaded by COVID-19 positive patients. When Sam shares his result, Jane receives an alert that she may have been exposed to COVID-19.
-                    </p>
-                </v-col> -->
-                <v-col cols="3">
-                    <img src="../assets/how_it_works/num-6-img.svg">
-                </v-col>
-            </v-row>
+                <Step right :img_num="require('../assets/how_it_works/num-6.svg')" :img_step="require('../assets/how_it_works/num-6-img.svg')" title="Exposure Notifications"  text="Jane’s phone checks the Covid Watch server for numbers uploaded by COVID-19 positive patients. When Sam shares his result, Jane receives an alert that she may have been exposed to COVID-19."></Step>
 
             <!-- Step 7 -->
-            <v-row class="py-5 px-12 d-flex align-end" id="step-2">
-                <Step :img="require('../assets/how_it_works/num-7.svg')" title="A Call to Action"  text="The alert tells Jane what day she was exposed and gives her the information she needs to take action. With this early warning, Jane is empowered to act quickly."></Step>
+                <Step :img_num="require('../assets/how_it_works/num-7.svg')" :img_step="require('../assets/how_it_works/num-7-img.svg')" title="A Call to Action"  text="The alert tells Jane what day she was exposed and gives her the information she needs to take action. With this early warning, Jane is empowered to act quickly."></Step>
 
-                <!-- <v-col cols="3">
-                    <img class="mb-4"  src="../assets/how_it_works/num-7.svg">
-                    <h3 class="mb-5"> A Call to Action </h3>
-                    <p>
-                   The alert tells Jane what day she was exposed and gives her the information she needs to take action. With this early warning, Jane is empowered to act quickly.
-                    </p>
-                </v-col> -->
-                <v-col cols="1">
-                </v-col>
-                <v-col cols="3">
-                    <img src="../assets/how_it_works/num-7-img.svg">
-                </v-col>
-            </v-row>
 
             <!-- Step 8 -->
             <v-row align="center"  justify="space-between" id="step-2">
@@ -193,8 +106,8 @@
 </template>
 
 <style lang="scss">
-#how-it-works{
 
+#how-it-works{
     @media (max-width:960px){
         .connected_img{
             display: none;
