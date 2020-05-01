c031c9eaee3adf49dd490358b438882c13416212
Jesse Colligan <jesse.m.colligan@gmail.com>
2020-04-30 20:35:16 -0400

    adjust svg in hero on /support

--- pages/support.vue
+++ pages/support.vue
@@ -14,9 +14,9 @@
             </div>
 
             <!-- inline style to be updated once we have global widths for Button component possibly -->
-            <div class="mt-16 pt-12 donate-container d-flex flex-wrap">
+            <div class="mt-16 pt-8 donate-container d-flex flex-wrap">
               <a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=W3U2KCLJGV9A8&source=url">
-                <Button style="width:300px;height:50px;font-weight:bold;" hero>Donate</Button>
+                <Button class="button-donate" hero>Donate</Button>
               </a>
               
               <img class="py-4" src="../assets/support_page/payment_options.svg" alt="payment">
@@ -26,12 +26,12 @@
           <v-spacer></v-spacer>
 
           <v-col :md="4" :sm="8">
-            <img style="max-height:370px;" src="../assets/support_page/people_globe.svg" alt="globe">
+            <img class="donate-svg" src="../assets/support_page/people_globe.svg" alt="globe">
           </v-col>
         </v-row>
 
         <!-- additional copy -->
-        <v-row>
+        <v-row class="mt-12 pt-6">
           <v-col>
             <h3>How We Use Donations</h3>
             <p class="mt-8">Funding for this project will be used primarily for app development and marketing. In terms of product development, funds will be used to support team members who are experiencing financial hardship and who require support to be able to continue working on app development, beta testing, and technical support; this would ensure more dedicated hours spent by developers on this project, resulting in higher-quality work being produced as soon as possible.</p>
@@ -56,8 +56,33 @@
 
       .hero-copy {
         margin-top: 50px;
-
       }
+
+      @media (max-width:400px){
+        .button-donate {
+          width: 280px;
+          height:50px;
+          font-weight:bold;
+        }
+
+        .donate-svg {
+            max-height:280px;
+        }
+        
+      }
+
+      @media (min-width:400px){
+        .button-donate {
+          width:300px;
+          height:50px;
+          font-weight:bold;
+        }
+
+        .donate-svg {
+            max-height:360px;
+        }
+      }
+
 
 
     .donate-container {
