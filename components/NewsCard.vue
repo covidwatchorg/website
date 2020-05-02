47091c3663c3d5b4e19b3ba45c7bbd30eec82bab
Colby Morrison <colbyamorrison@gmail.com>
2020-05-02 15:09:05 -0700

    add v-card-text component

--- components/NewsCard.vue
+++ components/NewsCard.vue
@@ -1,26 +1,41 @@
 <template>
-    <v-card :card="card" class="news-card">
-            <img class="stripe" style="position:absolute;" src="../assets/home_page/news_cards_top_accent.svg" alt="stripe">
-            <div class="d-flex py-6 px-6 justify-space-between">
-            <img src="../assets/home_page/news_icon.svg" alt=news>
-            <div class="news-date">{{card.date}}</div>
-            </div>
-            <div class="pa-6">
-            <a target="_blank" :href="card.url" class="primary--text" style="text-decoration:none;font-weight:bold;">{{card.title}} <img style="height:10px;" src="../assets/home_page/arrow_icon.svg" alt="arrow"></a>
-            <div class="primary--text float-right mt-10">
-                - {{card.author_name}}, <span style="font-style:italic;"> {{card.outlet_name}} </span>
-            </div>
-        </div>
+  <v-card :card="card" class="news-card">
+    <img
+      class="stripe"
+      style="position: absolute;"
+      src="../assets/home_page/news_cards_top_accent.svg"
+      alt="stripe"
+    />
+    <div class="d-flex py-6 px-6 justify-space-between">
+      <img src="../assets/home_page/news_icon.svg" alt="news" />
+      <div class="news-date">{{ card.date }}</div>
+    </div>
+    <div class="pa-6">
+      <a
+        target="_blank"
+        :href="card.url"
+        class="primary--text"
+        style="text-decoration: none; font-weight: bold;"
+        >{{ card.title }}
+        <img
+          style="height: 10px;"
+          src="../assets/home_page/arrow_icon.svg"
+          alt="arrow"
+      /></a>
+      <v-card-text class="primary--text grow float-right">
+        - {{ card.author_name }},
+        <span style="font-style: italic;"> {{ card.outlet_name }} </span>
+      </v-card-text>
+    </div>
 
-        <slot></slot>
-    </v-card>
+    <slot></slot>
+  </v-card>
 </template>
-
 
 <style lang="scss" scoped>
 .news-card {
-    height: 250px;
-    min-width: 320px;
+  height: 250px;
+  min-width: 320px;
 }
 
 //     @media (max-width:400px){
@@ -39,10 +54,10 @@
 </style>
 
 <script>
-    export default {
-    name: "NewsCard",
-    props: {
-        card: Object
-    }
-    };
+export default {
+  name: "NewsCard",
+  props: {
+    card: Object
+  }
+};
 </script>
