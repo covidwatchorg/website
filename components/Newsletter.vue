51f589167580458d0d91d8ea4b285d3cfaa70a69
Tessa Alexanian <hello@tessa.fyi>
2020-05-11 13:37:25 -0700

    wait, no, mailchimp has jquery, that's ok

--- components/Newsletter.vue
+++ components/Newsletter.vue
@@ -121,11 +121,6 @@
 </style>
 
 
-<script
-  src="https://code.jquery.com/jquery-3.5.1.min.js"
-  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
-  crossorigin="anonymous"
-></script>
 <script src="//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js"></script>
 <script>
 (function($) {
