b823326d8b03dfe7e667b2232e99a42eccdf9242
Colby Morrison <colbyamorrison@gmail.com>
2020-05-19 14:29:41 -0700

    update join us row

--- pages/about.vue
+++ pages/about.vue
@@ -73,7 +73,7 @@
 
       <!-- join us row -->
       <div class="join-us">
-      <v-row :class="{[pageSectionClass]: true, 'mx-3': $vuetify.breakpoint.smAndDown}" id="join-us">
+      <v-row :class="{[pageSectionClass]: true, 'mx-2': $vuetify.breakpoint.smAndDown}" id="join-us">
         <v-row class="mb-3">
           <v-col cols="10" :md="2">
             <h2>Join Us</h2>
