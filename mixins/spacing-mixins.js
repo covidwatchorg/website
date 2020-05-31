1c4462260aebe8c2e1dfb34cc25939e3b8a59215
Colby Morrison <colbyamorrison@gmail.com>
2020-05-31 00:37:51 -0700

    spacing mixin update

--- mixins/spacing-mixins.js
+++ mixins/spacing-mixins.js
@@ -1,7 +1,14 @@
 export default {
   data: () => ({
-    pageSectionClass: "pa-4 pa-sm-12",
     largeMarginClass: "mt-12 mb-6",
     flexCenterRowClass: "d-flex align-center justify-space-between",
   }),
+  computed: {
+      pageSectionClass: function(){
+          return{
+              "pa-4 pa-sm-12": true,
+              'mx-2': this.$vuetify.breakpoint.smAndDown
+          }
+      }
+  }
 };
