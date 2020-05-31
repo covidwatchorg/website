453f951648df0f967033896697948f4940df4434
Tessa Alexanian <hello@tessa.fyi>
2020-05-30 23:10:07 -0700

    Buttons looks as they ought to!

--- components/MenuButton.vue
+++ components/MenuButton.vue
@@ -3,42 +3,45 @@
     :nuxt="nuxt"
     :to="to"
     :href="href"
-    @click="$emit('click', $event)"
+    :value="value"
     text
+    :ripple="false"
     class="btn-flat-underlined py-4"
+    @click="$emit('click', $event)"
   >
     <slot></slot>
   </v-btn>
 </template>
             
 <style lang="scss" scoped>
-.btn-flat-underlined {
-  &.v-btn {
+.v-btn-toggle > .btn-flat-underlined.v-btn,
+.btn-flat-underlined.v-btn {
+  color: var(--v-primary-base);
+  font-weight: 600;
+  text-transform: none;
+  font-size: 20px;
+  border: none;
+
+  &:before {
+    display: none;
+  }
+
+  &:not(#mobile-menu-btn):hover:after {
+    @include pseudo-background(3px, rgb(67, 196, 217, 0.5));
+    width: calc(
+      100% - 32px
+    ); // by default, the buttons have 16px padding on either side
+  }
+
+  &--active {
     color: var(--v-primary-base);
-    font-weight: 600;
-    text-transform: none;
-    font-size: 20px;
+    font-weight: 700;
 
-    &:before {
-      display: none;
-    }
-
-    &:not(#mobile-menu-btn):hover:after {
-      @include pseudo-background(3px, rgb(67, 196, 217, 0.5));
+    &:after {
+      @include pseudo-background(3px, var(--v-primary-base));
       width: calc(
         100% - 32px
       ); // by default, the buttons have 16px padding on either side
-    }
-
-    &--active {
-      font-weight: 700;
-
-      &:after {
-        @include pseudo-background(3px, var(--v-primary-base));
-        width: calc(
-          100% - 32px
-        ); // by default, the buttons have 16px padding on either side
-      }
     }
   }
 }
@@ -50,7 +53,8 @@ export default {
   props: {
     href: String,
     to: String,
-    nuxt: Boolean
+    nuxt: Boolean,
+    value: [String, Number, Boolean]
   }
 };
 </script>
--- pages/news.vue
+++ pages/news.vue
@@ -7,31 +7,17 @@
             <h1 class="mb-5">News and Media</h1>
             <p>
               Want to reach our press team? Contact us at
-              <a href="mailto:media@covid-watch.org">media@covid-watch.org</a>.
+              <a
+                href="mailto:media@covid-watch.org"
+              >media@covid-watch.org</a>.
             </p>
-            <div id="filters" class="mt-10" style="max-width: 500px;">
-              <v-col :sm="8" :md="1">
-                <p class="title mb-0">Filter:</p>
-              </v-col>
-
-              <v-row id="filter-container">
-                <v-col :sm="8" :md="1">
-                  <Button secondary @click="toShow = 'all'" class="filter"
-                    >All News</Button
-                  >
-                </v-col>
-
-                <v-col :sm="8" :md="1">
-                  <Button secondary @click="toShow = 'mentions'" class="filter"
-                    >Mentions</Button
-                  >
-                </v-col>
-
-                <v-col :sm="8" :md="1">
-                  <Button secondary @click="toShow = 'releases'" class="filter"
-                    >Press Releases</Button
-                  >
-                </v-col>
+            <div class="mt-10" style="max-width: 500px;">
+              <v-row id="filter-container" justify="space-evenly">
+                <v-btn-toggle v-model="toShow" mandatory id="filter-group">
+                  <MenuButton value="all" class="filter">All News</MenuButton>
+                  <MenuButton value="mentions" class="filter">Mentions</MenuButton>
+                  <MenuButton value="releases" class="filter">Press Releases</MenuButton>
+                </v-btn-toggle>
               </v-row>
             </div>
           </v-col>
@@ -39,24 +25,14 @@
           <v-spacer></v-spacer>
 
           <v-col :sm="8" :md="4">
-            <img
-              id="bench-dude"
-              src="../assets/news_page/bench_dude.svg"
-              alt="news"
-            />
+            <img id="bench-dude" src="../assets/news_page/bench_dude.svg" alt="news" />
           </v-col>
         </v-row>
 
         <!-- all news cards section -->
         <v-row id="news-cards" class="mt-8">
           <!-- modularized NewsCard version -->
-          <v-col
-            :md="4"
-            :sm="6"
-            cols="12"
-            v-for="(card, i) in cardsToRender"
-            :key="i"
-          >
+          <v-col :md="4" :sm="6" cols="12" v-for="(card, i) in cardsToRender" :key="i">
             <NewsCard :card="card"></NewsCard>
           </v-col>
         </v-row>
@@ -72,9 +48,12 @@
 }
 
 #news {
-  #filters {
-    display: flex;
-    justify-content: space-evenly;
+  @import "~vuetify/src/styles/settings/_variables";
+
+  #filter-group {
+    @media #{map-get($display-breakpoints, 'sm-and-down')} {
+      flex-direction: column;
+    }
   }
 
   #bench-dude {
@@ -97,8 +76,9 @@
 </style>
 
 <script>
-import NewsCard from "../components/NewsCard.vue";
 import Button from "../components/Button.vue";
+import MenuButton from "../components/MenuButton.vue";
+import NewsCard from "../components/NewsCard.vue";
 import json from "../assets/data/medialist.json";
 import gsap from "gsap"; //this is a javascript animation library, more info: https://www.vuemastery.com/courses/animating-vue/intro-to-GSAP-3
 
@@ -110,21 +90,22 @@ export default {
       scale: 0.8,
       x: 0,
       ease: "power1",
-      stagger: 0.02,
+      stagger: 0.02
     });
   },
   components: {
-    NewsCard,
     Button,
+    MenuButton,
+    NewsCard
   },
-  updated: function () {
+  updated: function() {
     gsap.from(".news-card", {
       duration: 0.2,
       opacity: 0,
       scale: 0.8,
       x: 0,
       ease: "power1",
-      stagger: 0.02,
+      stagger: 0.02
     });
   },
   computed: {
@@ -132,24 +113,20 @@ export default {
       if (this.toShow === "all") {
         return this.airtableList;
       } else if (this.toShow === "mentions") {
-        return this.airtableList.filter(
-          (card) => card.type === "press_mention"
-        );
+        return this.airtableList.filter(card => card.type === "press_mention");
       } else {
-        return this.airtableList.filter(
-          (card) => card.type === "press_release"
-        );
+        return this.airtableList.filter(card => card.type === "press_release");
       }
-    },
+    }
   },
   data: () => ({
     airtableList: json.sort((a, b) => (a.date < b.date ? 1 : -1)),
-    toShow: "all",
+    toShow: "all"
   }),
   head() {
     return {
-      title: "News | Covid Watch",
+      title: "News | Covid Watch"
     };
-  },
+  }
 };
 </script>
