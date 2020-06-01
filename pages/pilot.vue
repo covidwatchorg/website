60fac7d966d140322a4956a160952b3f6299d282
David Lines <davidhenrylines@gmail.com>
2020-06-01 16:50:54 -0400

    Update pilot.vue
    Changing "exposure alert app" to "exposure notification app" since I believe that is the term we are using now.

--- pages/pilot.vue
+++ pages/pilot.vue
@@ -9,7 +9,7 @@
             <div class="subtitle">
               <p>
                 Covid Watch works best when communities adopt it together. Each
-                new person that uses our anonymous exposure alert app makes
+                new person that uses our anonymous exposure notification app makes
                 everyone else that much safer and reduces the burden on
                 overloaded public health systems.
               </p>
