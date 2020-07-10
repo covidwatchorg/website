fe93d03a6001ddebd5a8366a05c246e529b246ff
leon-i <leon-i@users.noreply.github.com>
2020-07-10 18:02:09 +0000

    Prettified Code!

--- pages/thank-you.vue
+++ pages/thank-you.vue
@@ -1,180 +1,185 @@
 <template>
   <v-container>
-  <v-row id="thank-you">
-    <v-col cols="12" class="py-0">
-      <!-- Header -->
-      <v-row id="header" :class="pageSectionClass">
-        <v-col cols="12" :md="7" :sm="12">
-          <div class="mb-12">
-            <h1 class="mb-5">Thank you! We couldn't do this without you.</h1>
-            <div class="subtitle">
-              <p>
-                Thank you for being part of the solution. Together we’ve created
-                a diverse community of researchers, developers, public health
-                professionals, security experts and more. Your contribution has
-                a tremendous impact, and we couldn’t thank you enough.
-              </p>
+    <v-row id="thank-you">
+      <v-col cols="12" class="py-0">
+        <!-- Header -->
+        <v-row id="header" :class="pageSectionClass">
+          <v-col cols="12" :md="7" :sm="12">
+            <div class="mb-12">
+              <h1 class="mb-5">Thank you! We couldn't do this without you.</h1>
+              <div class="subtitle">
+                <p>
+                  Thank you for being part of the solution. Together we’ve
+                  created a diverse community of researchers, developers, public
+                  health professionals, security experts and more. Your
+                  contribution has a tremendous impact, and we couldn’t thank
+                  you enough.
+                </p>
+              </div>
             </div>
-          </div>
-          <v-row class="d-flex justify-center">
-            <h3 class="mb4">Let's connect</h3>
-          </v-row>
-          <v-row class="d-flex justify-center">
-            <span class="group pa-2">
-              <a :href="github.link" class="ma-1">
-                <v-icon large class="blue-icon">mdi-github</v-icon>
-              </a>
-              <a :href="facebook.link" class="ma-1">
-                <v-icon large class="blue-icon">mdi-facebook</v-icon>
-              </a>
-              <a :href="twitter.link" class="ma-1">
-                <v-icon large class="blue-icon">mdi-twitter</v-icon>
-              </a>
-            </span>
-          </v-row>
-        </v-col>
-        <v-spacer class="col-1"></v-spacer>
-        <v-col cols="12" class="mt-12 mb-12" :sm="12" :md="4">
-          <div id="animal-img" class="placeholder"></div>
-        </v-col>
-      </v-row>
+            <v-row class="d-flex justify-center">
+              <h3 class="mb4">Let's connect</h3>
+            </v-row>
+            <v-row class="d-flex justify-center">
+              <span class="group pa-2">
+                <a :href="github.link" class="ma-1">
+                  <v-icon large class="blue-icon">mdi-github</v-icon>
+                </a>
+                <a :href="facebook.link" class="ma-1">
+                  <v-icon large class="blue-icon">mdi-facebook</v-icon>
+                </a>
+                <a :href="twitter.link" class="ma-1">
+                  <v-icon large class="blue-icon">mdi-twitter</v-icon>
+                </a>
+              </span>
+            </v-row>
+          </v-col>
+          <v-spacer class="col-1"></v-spacer>
+          <v-col cols="12" class="mt-12 mb-12" :sm="12" :md="4">
+            <div id="animal-img" class="placeholder"></div>
+          </v-col>
+        </v-row>
 
-      <!-- Volunteers section -->
-      <v-row id="volunteers" :class="pageSectionClass">
-        <v-col cols="12">
-          <div class="mb-12">
-            <h1 class="mb-5">Volunteers</h1>
-            <div class="subtitle">
-              <p>
-                Protecting our communities is a team effoert. Join us and become
-                part of the solution.
-              </p>
+        <!-- Volunteers section -->
+        <v-row id="volunteers" :class="pageSectionClass">
+          <v-col cols="12">
+            <div class="mb-12">
+              <h1 class="mb-5">Volunteers</h1>
+              <div class="subtitle">
+                <p>
+                  Protecting our communities is a team effoert. Join us and
+                  become part of the solution.
+                </p>
+              </div>
             </div>
-          </div>
-          <v-row class="align-center justify-space-between">
-            <v-icon large>mdi-chevron-left</v-icon>
+            <v-row class="align-center justify-space-between">
+              <v-icon large>mdi-chevron-left</v-icon>
               <v-col cols="8" :sm="8" :md="11">
                 <div id="animal-img-2" class="placeholder"></div>
               </v-col>
-            <v-icon large>mdi-chevron-right</v-icon>
-          </v-row>
-           <v-row class="justify-center">
+              <v-icon large>mdi-chevron-right</v-icon>
+            </v-row>
+            <v-row class="justify-center">
               <v-col cols="10" class="mt-12 mb-10 mb-md-0" :sm="8" :md="3">
                 <Button primary hero nuxt to="/join">Join Us</Button>
               </v-col>
             </v-row>
-        </v-col>
-      </v-row>
+          </v-col>
+        </v-row>
 
-      <!-- Research participants section -->
-      <v-row id="research-participants" :class="pageSectionClass">
-        <v-col cols="12">
-          <div class="mb-12">
-            <h1 class="mb-5">Research Participants</h1>
-            <div class="subtitle">
-              <p>
-                Want to shape the future of the app? Sign up below and become a
-                research participant.
-              </p>
+        <!-- Research participants section -->
+        <v-row id="research-participants" :class="pageSectionClass">
+          <v-col cols="12">
+            <div class="mb-12">
+              <h1 class="mb-5">Research Participants</h1>
+              <div class="subtitle">
+                <p>
+                  Want to shape the future of the app? Sign up below and become
+                  a research participant.
+                </p>
+              </div>
             </div>
-          </div>
-          <v-row class="justify-center">
-            <v-col cols="12" :sm="10" :md="12">
-              <div id="animal-img-3" class="placeholder"></div>
-            </v-col>
-          </v-row>
-          <v-row class="d-flex align-center justify-center">
-            <v-col cols="10" class="mt-12 mb-10 mb-md-0" :sm="8" :md="3">
-              <Button primary hero nuxt to="/join">Sign up</Button>
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
+            <v-row class="justify-center">
+              <v-col cols="12" :sm="10" :md="12">
+                <div id="animal-img-3" class="placeholder"></div>
+              </v-col>
+            </v-row>
+            <v-row class="d-flex align-center justify-center">
+              <v-col cols="10" class="mt-12 mb-10 mb-md-0" :sm="8" :md="3">
+                <Button primary hero nuxt to="/join">Sign up</Button>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
 
-      <!-- Pilot partners section -->
-      <v-row id="pilot-partners" :class="pageSectionClass">
-        <v-col cols="12">
-          <div class="mb-12">
-            <h1 class="mb-5">Pilot Partners</h1>
-            <div class="subtitle">
-              <p>
-                Want to bring Covid Watch to your community? Learn how to start
-                a pilot.
-              </p>
+        <!-- Pilot partners section -->
+        <v-row id="pilot-partners" :class="pageSectionClass">
+          <v-col cols="12">
+            <div class="mb-12">
+              <h1 class="mb-5">Pilot Partners</h1>
+              <div class="subtitle">
+                <p>
+                  Want to bring Covid Watch to your community? Learn how to
+                  start a pilot.
+                </p>
+              </div>
             </div>
-          </div>
-          <v-row class="justify-start">
-            <v-col cols="4" :sm="6" :md="4">
-              <div id="partner-img" class="placeholder"></div>
-            </v-col>
-          </v-row>
-          <v-row class="d-flex align-center justify-center">
-            <v-col cols="10" class="mt-12 mb-10 mb-md-0" :sm="8" :md="3">
-              <Button primary hero nuxt to="/pilot">Start a pilot</Button>
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
+            <v-row class="justify-start">
+              <v-col cols="4" :sm="6" :md="4">
+                <div id="partner-img" class="placeholder"></div>
+              </v-col>
+            </v-row>
+            <v-row class="d-flex align-center justify-center">
+              <v-col cols="10" class="mt-12 mb-10 mb-md-0" :sm="8" :md="3">
+                <Button primary hero nuxt to="/pilot">Start a pilot</Button>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
 
-      <!-- Companies section -->
-      <v-row id="companies" :class="pageSectionClass">
-        <v-col cols="12">
-          <div class="mb-12">
-            <h1 class="mb-5">Companies</h1>
-            <div class="subtitle">
-              <p>
-                Does your organization want to work with us to help stop the
-                spread of COVID-19? Get in touch.__
-              </p>
+        <!-- Companies section -->
+        <v-row id="companies" :class="pageSectionClass">
+          <v-col cols="12">
+            <div class="mb-12">
+              <h1 class="mb-5">Companies</h1>
+              <div class="subtitle">
+                <p>
+                  Does your organization want to work with us to help stop the
+                  spread of COVID-19? Get in touch.__
+                </p>
+              </div>
             </div>
-          </div>
-          <v-row class="justify-start">
-            <v-col cols='10' :sm="12" :md="10">
-              <div id="companies-img" class="placeholder"></div>
-            </v-col>
-          </v-row>
-          <v-row class="d-flex align-center justify-center">
-            <v-col cols="10" class="mt-12 mb-10 mb-md-0" :sm="8" :md="3">
-              <Button primary hero nuxt to="/join">Collaborate</Button>
-            </v-col>
-          </v-row>
-        </v-col>
-      </v-row>
+            <v-row class="justify-start">
+              <v-col cols="10" :sm="12" :md="10">
+                <div id="companies-img" class="placeholder"></div>
+              </v-col>
+            </v-row>
+            <v-row class="d-flex align-center justify-center">
+              <v-col cols="10" class="mt-12 mb-10 mb-md-0" :sm="8" :md="3">
+                <Button primary hero nuxt to="/join">Collaborate</Button>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-row>
 
-      <!-- Connect with us section -->
-      <v-row id="connect-with-us" :class="pageSectionClass">
-        <v-col cols="12">
-          <div class="mb-12">
-            <h2 class="mb-8">Connect With Us</h2>
-            <div class="subtitle">
-              <p>
-                Do you want to help us tell our story? Does your organization
-                want to work with us to help stop the spread of COVID-19? Get in
-                touch:
-              </p>
+        <!-- Connect with us section -->
+        <v-row id="connect-with-us" :class="pageSectionClass">
+          <v-col cols="12">
+            <div class="mb-12">
+              <h2 class="mb-8">Connect With Us</h2>
+              <div class="subtitle">
+                <p>
+                  Do you want to help us tell our story? Does your organization
+                  want to work with us to help stop the spread of COVID-19? Get
+                  in touch:
+                </p>
+              </div>
             </div>
-          </div>
-          <ul class="contact-list">
-            <li>
-              Media inquiries:
-              <a href="mailto: media@covidwatch.org">media@covidwatch.org</a>
-            </li>
-            <li>
-              Partnerships:
-              <a href="mailto: contact@covidwatch.org">partnerships@covidwatch.org</a>
-            </li>
-            <li>
-              All other inquiries:
-              <a href="mailto: contact@covidwatch.org">contact@covidwatch.org</a>
-            </li>
-          </ul>
-        </v-col>
-      </v-row>
+            <ul class="contact-list">
+              <li>
+                Media inquiries:
+                <a href="mailto: media@covidwatch.org">media@covidwatch.org</a>
+              </li>
+              <li>
+                Partnerships:
+                <a href="mailto: contact@covidwatch.org"
+                  >partnerships@covidwatch.org</a
+                >
+              </li>
+              <li>
+                All other inquiries:
+                <a href="mailto: contact@covidwatch.org"
+                  >contact@covidwatch.org</a
+                >
+              </li>
+            </ul>
+          </v-col>
+        </v-row>
 
-      <!-- Newsletter -->
-      <Newsletter></Newsletter>
-    </v-col>
-  </v-row>
+        <!-- Newsletter -->
+        <Newsletter></Newsletter>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -236,7 +241,7 @@
   }
 
   .contact-list a {
-    color: #2C58B1;
+    color: #2c58b1;
   }
 
   .bold {
@@ -288,7 +293,7 @@
 <script>
 import Button from "../components/Button";
 import CTA from "../components/CTA";
-import Newsletter from '../components/Newsletter';
+import Newsletter from "../components/Newsletter";
 import {
   TWITTER,
   FACEBOOK,
@@ -300,7 +305,7 @@ export default {
   components: {
     Button,
     CTA,
-    Newsletter
+    Newsletter,
   },
   head() {
     return {
