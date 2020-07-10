fbd6745c012575e088be7d4a6bf126dd8d4593de
Jesse Colligan <jesse.m.colligan@gmail.com>
2020-07-09 18:16:50 -0700

    add open positions google doc to /join

--- pages/join.vue
+++ pages/join.vue
@@ -50,6 +50,12 @@
               >
             </v-col>
 
+            <p>
+              <strong>We also have some open positions!</strong>
+              Check them out <a href="https://docs.google.com/document/d/1OsxqgCZYk48jblVZCYSblm06A1IqPbjTHPBk0lvFo1s/edit">here</a>.
+            </p>
+            
+
             <h4 class="mt-6 mb-4">Most Wanted Volunteers</h4>
             <ul>
               <li v-for="(item, i) in urgentRoles" :key="i">
